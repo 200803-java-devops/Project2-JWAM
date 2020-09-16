ad6bba4df58be4708b9459e675ac17074530fb5f
Alxl <44034025+ItsAlxl@users.noreply.github.com>
2020-09-16 10:57:30 -0500

    Target app deployment

--- gimbus2-jenkins/BuildAndPush.sh
+++ gimbus2-jenkins/BuildAndPush.sh
@@ -1,4 +1,4 @@
 echo "Enter Docker Hub username"
 read username
-docker build -t $username/gimbus2-jenkins:latest .
+docker build -t $username/gimbus2-jenkins:latest $(dirname $0)
 docker push $username/gimbus2-jenkins:latest
